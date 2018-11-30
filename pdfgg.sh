612e4d4db35cb0ce8a0fad1166362cab342d8e60
Lung Sin <31788267+lungsin@users.noreply.github.com>
2018-11-30 16:19:58 +0800

    Update pdfgg.sh

--- pdfgg.sh
+++ pdfgg.sh
@@ -1,17 +1,19 @@
 # Format in std input:
-# <namepdf>.pdf cat <page> <page> <page> <page> <...> <page>
+# <namepdf>.pdf <pages separated by space>
 # example: 
-# Lecture1.pdf cat 10 12 13 40 
+# Lecture1.pdf 10 12 13 20-25  
 
 id=0
 merge=""
 while read i; do
 	id=`expr $id + 1`
-	echo " $id => processing $i"
-	pdftk $i output tmp$id.pdf
+	f_name=$(echo $i | cut -d" " -f1-1)
+	pages=$(echo $i | cut -d" " -f2-)
+	echo " $id => processing $f_name with pages $pages"
+	pdftk $f_name cat $pages output tmp$id.pdf
 	merge=$(echo "$merge tmp$id.pdf")
 done
 
 pdftk $merge output out.pdf
-pdfnup --nup 2x4 --suffix 'nup' --no-landscape --frame true --batch out.pdf
+pdfnup --nup 2x4 --suffix 'nup' --no-landscape --frame true --scale 0.95 --batch out.pdf
 rm tmp*
